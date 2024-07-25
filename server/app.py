41c6a78f53d522967104b6302162e22fdda3a52e
Kamau <kamauwamuigaijr@gmail.com>
2024-07-25 16:41:29 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -16,40 +15,40 @@ db.init_app(app)
 
 api = Api(app)
 
-
-class Plants(Resource):
-
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
-
-    def post(self):
-        data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
 class PlantByID(Resource):
 
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+    def patch(self, id):
+        record = Plant.query.filter_by(id=id).first()
+        if not record:
+            return make_response({"error": "Plant not found"}, 404)
 
+        data = request.get_json()
+        for attr in data:
+            setattr(record, attr, data[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+        return response
+
+    def delete(self, id):
+        record = Plant.query.filter_by(id=id).first()
+        if not record:
+            return make_response({"error": "Plant not found"}, 404)
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response = make_response("", 204)
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
